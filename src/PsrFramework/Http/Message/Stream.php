7847878f06f1ab7c9b0e9229e013f02811963cf7
Vilena <vilena.saitgalina@mail.ru>
2024-04-07 11:50:18 +0500

    Add stream, uploadedFile

--- src/PsrFramework/Http/Message/Stream.php
+++ src/PsrFramework/Http/Message/Stream.php
@@ -2,83 +2,137 @@
 
 namespace Csu\PsrFramework\Http\Message;
 
+use http\Exception\RuntimeException;
 use Psr\Http\Message\StreamInterface;
 
 class Stream implements StreamInterface
 {
-
+    private $stream;
+    public function __construct($stream)
+    {
+        $this -> $stream = $stream;
+    }
     public function __toString(): string
     {
-        // TODO: Implement __toString() method.
+        if (!$this->isSeekable()){
+            return '';
+        }
+        $this -> rewind();
+        return stream_get_contents($this -> stream);
     }
 
     public function close(): void
     {
-        // TODO: Implement close() method.
+        fclose($this -> stream);
     }
 
     public function detach()
     {
-        // TODO: Implement detach() method.
+        $result = $this -> stream;
+        $this -> stream = null;
+        return $result;
     }
 
     public function getSize(): ?int
     {
-        // TODO: Implement getSize() method.
+        if(is_resource($this->stream)){
+            $stats = fstat($this->stream);
+            return isset($stats['size']) ? $stats['size'] : null;
+        }
+       return null;
     }
 
     public function tell(): int
     {
-        // TODO: Implement tell() method.
+        if (!is_resource($this->stream)){
+            throw new RuntimeException("Stream is not a resource");
+        }
+        $result = ftell($this->stream);
+        if (!is_int($result)){
+            throw new RuntimeException("Error occurred during tell operation");
+        }
+        return $result;
     }
 
     public function eof(): bool
     {
-        // TODO: Implement eof() method.
+        return !$this->stream || $this->eof($this->stream);
     }
 
     public function isSeekable(): bool
     {
-        // TODO: Implement isSeekable() method.
+        $meta = stream_get_meta_data($this->stream);
+        return $meta['seekable'];
     }
 
     public function seek(int $offset, int $whence = SEEK_SET): void
     {
-        // TODO: Implement seek() method.
+        if(!$this->isSeekable()){
+            throw new RuntimeException("Stream is not seekable");
+        }
+        fseek($this->stream, $offset, $whence);
     }
 
     public function rewind(): void
     {
-        // TODO: Implement rewind() method.
+        $this->seek(0);
     }
 
     public function isWritable(): bool
     {
-        // TODO: Implement isWritable() method.
+        $meta = stream_get_meta_data($this->stream);
+        $mode = $meta['mode'];
+        return strpos($mode, 'w') !== false
+            || strpos($mode, 'a') !== false
+            || strpos($mode, 'x') !== false
+            || strpos($mode, 'c') !== false
+            || strpos($mode, '+') !== false;
     }
 
     public function write(string $string): int
     {
-        // TODO: Implement write() method.
+        if(!$this->isWritable()){
+            throw new RuntimeException("Stream is not writable");
+        }
+        $result = fwrite($this->stream);
+        if ($result===false){
+            throw new RuntimeException("Error writing to stream");
+        }
+        return $result;
     }
 
     public function isReadable(): bool
     {
-        // TODO: Implement isReadable() method.
+        $meta = stream_get_meta_data($this->stream);
+        $mode = $meta['mode'];
+        return strpos($mode, 'r') !== false
+            || strpos($mode, '+') !== false;
     }
 
     public function read(int $length): string
     {
-        // TODO: Implement read() method.
+        if(!$this->isReadable()){
+            throw new RuntimeException("Stream is not readable");
+        }
+        $result = fread($this->stream);
+        return $result !== false ? $result : '';
     }
 
     public function getContents(): string
     {
-        // TODO: Implement getContents() method.
+        if (!$this->isReadable()){
+            throw new RuntimeException("Stream is not readable");
+        }
+        $contents = stream_get_contents($this->stream);
+        return $contents !== false ? $contents : '';
     }
 
     public function getMetadata(?string $key = null)
     {
-        // TODO: Implement getMetadata() method.
+        $meta = stream_get_meta_data($this->stream);
+        if ($key === null){
+            return $meta;
+        }
+        return isset($meta[$key]) ? $meta[$key] : null;
     }
 }
